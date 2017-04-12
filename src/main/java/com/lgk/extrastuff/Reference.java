2f7f8a255283573b62002fe0407d813e365a3067
Lgk2 <dunngen3@live.se>
2017-04-12 23:45:30 +0200

    Fixed reference.

--- src/main/java/com/lgk/extrastuff/Reference.java
+++ src/main/java/com/lgk/extrastuff/Reference.java
@@ -8,6 +8,6 @@ public class Reference {
     public static final String MODID = "extrastuff";
     public static final String NAME = "Extra Stuff";
     public static final String VERSION = "1.0";
-    public static final String SERVER_PROXY_CLASS = "CommonProxy";
-    public static final String CLIENT_PROXY_CLASS = "ClientProxy";
+    public static final String SERVER_PROXY_CLASS = "com.lgk.extrastuff.proxy.CommonProxy";
+    public static final String CLIENT_PROXY_CLASS = "com.lgk.extrastuff.proxy.ClientProxy";
 }
